c4a9c1bef5e44823e5c9b5d88db06982edf386d6
Elturriza <ghturriza@gmail.com>
2020-02-29 23:56:46 -0600

    mecos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import styled from "styled-components";
+const Abe = styled.h1`
+color: red;
+border: 1px solid red;
+background: #eee;
 
+`;
 export default () => {
   return (
     <div>
       <ul>
         <h3>gey</h3>
+        <Abe>a</Abe>
         <li>
           <Link href="/blogs/f">f</Link>
         </li>

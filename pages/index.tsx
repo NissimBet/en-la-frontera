4ea01b745dcd03a1259b95732811dd8a517418bb
Elturriza <ghturriza@gmail.com>
2020-02-29 22:23:40 -0600

    otra prueba

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ export default () => {
       <ul>
 
       <li><Link href="/blogs/2">2</Link></li>
+      <li><Link href="/blogs/2">2</Link></li>
+      <li><Link href="/blogs/2">2</Link></li>
       <li><Link href="/blogs/f">f</Link></li>
       <li><Link href="/blogs/43">43</Link></li>
       <li><Link href="/blogs/55">55</Link></li>

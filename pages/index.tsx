74b502d5534a07e1619fb3a54b64c69c7f6aa7b1
NissimBet <nissimbet123@outlook.com>
2020-02-29 22:25:29 -0600

    cambios mecos

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,19 @@ export default () => {
   return (
     <div>
       <ul>
-
-      <li><Link href="/blogs/2">2</Link></li>
-      <li><Link href="/blogs/f">f</Link></li>
-      <li><Link href="/blogs/43">43</Link></li>
-      <li><Link href="/blogs/55">55</Link></li>
+        <h3>gey</h3>
+        <li>
+          <Link href="/blogs/f">f</Link>
+        </li>
+        <li>
+          <Link href="/blogs/2">2</Link>
+        </li>
+        <li>
+          <Link href="/blogs/43">43</Link>
+        </li>
+        <li>
+          <Link href="/blogs/55">55</Link>
+        </li>
       </ul>
       <p>hello</p>
     </div>
